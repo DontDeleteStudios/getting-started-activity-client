511bbc3b3c513b19d15761f1226eea6064d78315
Nathaniel Rand <nathanieljrand@gmail.com>
2024-04-27 01:20:02 -0400

    fourth client

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://getting-started-activity-server-7e0q9tveg.vercel.app',
+        target: 'https://getting-started-activity-server-7e0q9tveg.vercel.app:3001',
         changeOrigin: true,
         secure: false,
         ws: true,
