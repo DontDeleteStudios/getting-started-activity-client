ac1b3f38d0266d82f69c5aa8ddadf85596acbaf8
Nathaniel Rand <nathanieljrand@gmail.com>
2024-04-27 01:10:20 -0400

    second client

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import {defineConfig} from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  envDir: '../',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://getting-started-activity-pink.vercel.app:3001',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+    hmr: {
+      clientPort: 443,
+    },
+  },
+});
