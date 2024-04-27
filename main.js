31d99a5f067d0ec2b071cc387c5ee9a39ccc49f8
Nathaniel Rand <nathanieljrand@gmail.com>
2024-04-27 11:40:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { DiscordSDK } from "@discord/embedded-app-sdk";
 
 import rocketLogo from '/rocket.png';
 import "./style.css";
-//weeeeeeee
+
 // Will eventually store the authenticated user's access_token
 let auth;
 
